9affd211a49d0543066571b2fc3ff2894c09203c
Linh Nguyen <nguyenphuclinh3010@gmail.com>
2022-08-11 22:18:34 +0700

    Configure ESLint

--- AppProviders.js
+++ AppProviders.js
@@ -1,7 +1,13 @@
-import { TailwindProvider } from "tailwindcss-react-native";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TailwindProvider } from 'tailwindcss-react-native';
 
 const AppProviders = ({ children }) => {
-    return <TailwindProvider>{children}</TailwindProvider>;
-}
+  return <TailwindProvider>{children}</TailwindProvider>;
+};
+
+AppProviders.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default AppProviders;
